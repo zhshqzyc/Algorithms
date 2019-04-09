37c75627dab5c6c34924e884c937a874dd3443d6
Hui Zhao <zhshqzyc@gmail.com>
2019-04-09 12:02:47 -0400

    Add files via upload

---
+++ BubbleSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[7] { 64, 34, 25, 12, 22, 11, 90 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                        Swap(arr, i, j);
+                }
+            }
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
