92a8705eea12b7316b936ab43a4f46424d299244
zhshqzyc <zhshqzyc@gmail.com>
2019-04-09 10:49:45 -0400

    Add files via upload

--- MergeSort.cs
+++ MergeSort.cs
@@ -1,77 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MergeSort
 {
-    // https://www.youtube.com/watch?v=TzeBrDU-JaY
-    class Program
+    class MergeSort
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[] { 12, 11, 13, 5, 6, 7 };
-            MergeSort(arr);
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.WriteLine(arr[i]);
-            }
-            Console.ReadKey();
-        }
-
-        private static void MergeSort(int[] arr)
-        {
-            int n = arr.Length;
-            if (n < 2)
-                return;
-            int mid = n / 2;
-            // left  <-- array of size(mid);
-            // right <-- array of size(n-mid);
-            int[] left = new int[mid];
-            int[] right = new int[n - mid];
-            for (int i = 0; i < mid; i++)
-            {
-                left[i] = arr[i];
-            }
-            for (int i = mid; i < n; i++)
-            {
-                right[i - mid] = arr[i];
-            }
-            MergeSort(left);
-            MergeSort(right);
-            Merge(arr, left, right);
-        }
-
-        private static void Merge(int[] arr, int[] left, int[] right)
-        {
-            int nL = left.Length;
-            int nR = right.Length;
-            int i = 0;
-            int j = 0;
-            int k = 0;
-            while (i < nL && j < nR)
-            {
-                if (left[i] <= right[j])
-                {
-                    arr[k] = left[i];
-                    i++;
-                }
-                else
-                {
-                    arr[k] = right[j];
-                    j++;
-                }
-                k++;
-            }
-            while (i < nL)
-            {
-                arr[k] = left[i];
-                i++;
-                k++;
-            }
-            while (j < nR)
-            {
-                arr[k] = right[j];
-                j++;
-                k++;
-            }
+            int[] arr = new int[7] { 64, 34, 25, 12, 22, 11, 90 };
         }
     }
 }
